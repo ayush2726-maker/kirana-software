f159c464455256befdc01a182375d364322f2684
ayush2726-maker <ayush2726@gmail.com>
2026-08-08 11:22:55 +0530

    Bump owner release for smart billing tools

---
+++ backend/release_135_ext.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+import backend.native_owner_app_ext as native_owner
+import backend.owner_final_inline_ext as final_owner
+import backend.stable_owner_app_ext as stable_owner
+
+
+VERSION = "135"
+native_owner.BUILD = VERSION
+final_owner.BUILD = VERSION
+stable_owner.VERSION = VERSION
